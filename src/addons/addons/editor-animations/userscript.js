4f12cf982b3913c352e00b5ac5cf14735c9d419e
mark <94125865+mmmmaaaaarrrrrrkkkkkkkk@users.noreply.github.com>
2025-07-16 13:18:07 -0500

    github being really weird maybe this commit will fix????

--- src/addons/addons/editor-animations/userscript.js
+++ src/addons/addons/editor-animations/userscript.js
@@ -40,7 +40,8 @@ export default async function({ addon }) {
     "emphasis": "linear(0 0%, 0 1.8%, 0.01 3.6%, 0.03 6.35%, 0.07 9.1%, 0.13 11.4%, 0.19 13.4%, 0.27 15%, 0.34 16.1%, 0.54 18.35%, 0.66 20.6%, 0.72 22.4%, 0.77 24.6%, 0.81 27.3%, 0.85 30.4%, 0.88 35.1%, 0.92 40.6%, 0.94 47.2%, 0.96 55%, 0.98 64%, 0.99 74.4%, 1 86.4%, 1 100%)",
   };
   const hasNoVariation = ["default", "fastInSlowOut", "smoothStep", "elastic", "bounce", "emphasis"];
-
+  
+  
   let needsInit = true, animateModals = true, animateLibraries = true, animateButtons = true,
     animationSpeed = 1, animationType = "default", animationDir = "inOut";
   let patchedBody = false, sbPatched = false, sbEverPatched = false, listenerAttached = false;
